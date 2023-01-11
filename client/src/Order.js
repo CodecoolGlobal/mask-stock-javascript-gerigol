e9e10986627badc5d4f97f1d903332ae1cdccf1b
Golen Gerhárd <golengerhard@gmail.com>
2023-01-11 11:16:15 +0100

    fix: order value can't be less then 1

--- client/src/Order.js
+++ client/src/Order.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 
 
 const Order = () => {
+  const [orderNumber, setOrderNumber] = useState(1);
+  const handleChange = (value) => {
+    value > 0 && setOrderNumber(value)
+  }
 
   return (
     <div>
       <form >
         <label htmlFor="masks">Mask order number: </label>
-        <input type="number" name="masks" />
+        <input type="number" name="masks" onChange={(e) => handleChange(e.target.value)} value={orderNumber} />
         <button >Place order</button>
+        <p>{orderNumber}</p>
       </form>
     </div>
   )

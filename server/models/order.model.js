78a5fa7e6f5ea7f465bf596b3983344c3d786cf4
Golen Gerhárd <golengerhard@gmail.com>
2023-01-11 11:06:28 +0100

    modify order model

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,56 +1,11 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
-  vendor_id: String, // hospital id
-  partner_id: Number, // user id ?
-  block_id: Number,
-  bank_account_id: Number,
-  type: String,
-  fulfillment_date: String,
-  due_date: String,
-  payment_method: String,
-  language: String,
-  currency: String,
-  conversion_rate: Number,
-  electronic: Boolean,
-  paid: Boolean,
-  items: [
-    {
-      product_id: Number,
-      quantity: Number,
-      comment: String
-    },
-    {
-      name: String,
-      unit_price: Number,
-      unit_price_type: String,
-      quantity: Number,
-      unit: String,
-      vat: String,
-      comment: String,
-      entitlement: String
-    }
-  ],
-  comment: String,
-  settings: {
-    mediated_service: Boolean,
-    without_financial_fulfillment: Boolean,
-    online_payment: String,
-    round: String,
-    no_send_onlineszamla_by_user: Boolean,
-    order_number: String,
-    place_id: Number,
-    instant_payment: Boolean,
-    selected_type: String
-  },
-  advance_invoice: [
-    Number
-  ],
-  discount: {
-    type: String,
-    value: Number
-  },
-  instant_payment: Boolean
+const { Schema } = mongoose
+
+const orderSchema = new Schema({
+  amount: Number,
+  price: Number,
+  partner: { type: Schema.Types.ObjectId, ref: 'User' }
 })
 
 module.exports = mongoose.model('Order', orderSchema)

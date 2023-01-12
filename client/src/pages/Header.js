7c6e9eaf650d7171e40a0e7f6e8a6611a5a0b3b7
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-01-12 13:48:05 +0100

    feat: minor css

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,14 +1,15 @@
 const Header = () => {
-  return (
-    <div
-      style={{
+    const headerStyle = {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         height: "10vh",
         backgroundColor: "antiquewhite",
         fontSize: "45px",
-      }}
+      }
+  return (
+    <div
+      style={headerStyle}
     >
       Welcome to our page!
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,46 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const h1Style = {
+  margin: "15px",
+  backgroundColor: "yellow",
+  width: "10%",
+};
 
 const Home = () => {
-  const [products, setProducts] = useState(null)
+  const [products, setProducts] = useState(null);
 
   useEffect(() => {
-    fetch('api/products')
-      .then(res => res.json())
-      .then(data => setProducts(data))
+    fetch("api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-  }, [])
-
-  if (!products) return (<h1>Loading...</h1>);
+  if (!products) return <h1>Loading...</h1>;
 
   return (
     <div>
       <div>
-        <h1>Products:</h1>
+        <h1 style={h1Style}>Products:</h1>
       </div>
-      <Link to={'/order'}>
-        <div className="card" style={{ backgroundColor: '#C0C0C0', width: '500px', borderRadius: '30px', marginLeft: '30px' }}>
+      <Link to={"/order"}>
+        <div
+          className="card"
+          style={{
+            backgroundColor: "#C0C0C0",
+            width: "500px",
+            borderRadius: "30px",
+            marginLeft: "30px",
+          }}
+        >
           <div>
-            <img src="./musk.jpg" alt="" style={{ width: '500px', borderRadius: '30px' }} />
+            <img
+              src="./musk.jpg"
+              alt=""
+              style={{ width: "500px", borderRadius: "30px" }}
+            />
           </div>
-          <div style={{ padding: '20px' }}>
+          <div style={{ padding: "20px" }}>
             <h2>Name: Mask</h2>
             <h2>In Stock : {products[0].inStock}</h2>
           </div>
@@ -31,6 +48,6 @@ const Home = () => {
       </Link>
     </div>
   );
-}
+};
 
 export default Home;
